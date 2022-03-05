9dd869c70b8554b52a2ee3b8fbca5796672e6d48
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-03-05 19:51:46 -0300

    reset

--- next.config.js
+++ next.config.js
@@ -1,36 +1,6 @@
-import remarkFrontmatter from 'remark-frontmatter';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
-
-export default {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          options: {
-            providerImportSource: '@mdx-js/react',
-            remarkPlugins: [remarkFrontmatter],
-          },
-        },
-      ],
-    })
-  }
-}
-
-export default {
-  //..
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  images: {
-    loader: 'imgix',
-    path: 'https://images.unsplash.com/',
-  },
-  //...
-};
